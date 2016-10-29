e66bbea26254d3471fc5fad10164abd8524fe7e9
KrissuEST <kristjantonismae@gmail.com>
2016-10-29 17:31:34 +0300

    Third day added.

---
+++ src/paev3/ArvudJarjest.java
@@ -0,0 +1,60 @@
+package paev3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import lib.TextIO;
+
+public class ArvudJarjest {
+	
+	//Teha ise sorteerimise algoritm. Näpuharjutus.
+	public static void sort(ArrayList<Integer> arvud)
+	{
+		//Võib vaja olla isegi kaks for tsüklit.
+//		for (int i = 0; i < args.length; i++) {
+//			
+//		}
+	}
+	
+	public static void main(String[] args) {
+		
+		//------ Mite objektorienteeritud kood -----
+		int mitu = 3;
+		
+		ArrayList<Sportlane> sportlased = new ArrayList<>();
+		
+		for (int i = 0; i < mitu; i++) {
+			Sportlane sportlane = new Sportlane(nimi, tulemus);
+			System.out.println("Nimi: ");
+			sportlane.nimi = TextIO.getlnString();
+			System.out.println(sportlane.nimi + " heitis ketast: ");
+			sportlane.tulemus = TextIO.getlnDouble();
+
+			sportlased.add(sportlane);
+		}
+		
+		Collections.sort(sportlased);;
+		
+		//Välja trükkimine.
+		for (Sportlane sportlane : sportlased) {
+			System.out.println(sportlane);
+		}
+	}
+	
+	//------ Mite nii objektorienteeritud kood -----
+//		System.out.println("Sisesta palun " + mitu + " arvu");
+//		
+//		ArrayList<Double> arvud = new ArrayList<Double>();
+//		
+//		for (int i = 0; i < mitu; i++) {
+//			double arv = TextIO.getlnDouble();
+//			arvud.add(arv);
+//		}
+//		
+//		Collections.sort(arvud);
+//		
+//		//Välja trükkimine.
+//		for (Double arv : arvud) {
+//			System.out.println(arv + " ");
+//		}
+	}
